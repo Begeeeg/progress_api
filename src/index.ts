97351424dcc076dd3eca68c853a1adf58a7f75e9
Begeeeg <v.yecyec7@gmail.com>
2026-09-05 15:56:39 +0800

    🔰 add(src): app, router, and index added

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config({ quiet: true });
+
+import app from "./app";
+
+const PORT = process.env.PORT || 5000;
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`🛫 Server is running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error("💥 Failed to start server:", error);
+        process.exit(1);
+    }
+};
+
+start();
