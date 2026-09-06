756488fab8c73fb3bc2124d1a5224e86d372d783
Begeeeg <v.yecyec7@gmail.com>
2026-09-06 17:32:57 +0800

    ⏫ up(router): auth router added

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { authRouter } from "./feature/v1/identity/auth";
 
 const router = Router();
+
+router.use("/identity/auth", authRouter);
 
 export default router;
