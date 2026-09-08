3b1d13df323f61ba8eff5f9b1071af964b397e4f
Begeeeg <v.yecyec7@gmail.com>
2026-09-08 13:06:42 +0800

    ⏫ up(auth): implement rateLimiter on the router

--- src/feature/v1/identity/auth/auth.router.ts
+++ src/feature/v1/identity/auth/auth.router.ts
@@ -10,13 +10,24 @@ import { validate } from "../../../../common/middleware/validatorDataDto";
 import { RegisterUserSchema } from "./dtos/register.data.dto";
 import { requireAuth } from "../../../../common/middleware/requireAuth";
 import { LoginUserSchema } from "./dtos/login.data.dto";
+import { authLimiter } from "../../../../common/middleware/rateLimiter";
 
 const router = express.Router();
 
-router.post("/register", validate(RegisterUserSchema), registerController);
+router.post(
+    "/register",
+    authLimiter,
+    validate(RegisterUserSchema),
+    registerController
+);
 router.get("/verify-email", verifyEmailController);
-router.post("/resend-verification", requireAuth, resendVerificationController);
+router.post(
+    "/resend-verification",
+    authLimiter,
+    requireAuth,
+    resendVerificationController
+);
 router.post("/logout", requireAuth, logOutController);
-router.post("/login", validate(LoginUserSchema), logInController);
+router.post("/login", authLimiter, validate(LoginUserSchema), logInController);
 
 export default router;
