2a4a44f84478288cd09eb8683a1da26ccbe39f07
Begeeeg <v.yecyec7@gmail.com>
2026-09-14 17:07:27 +0800

    ⏫ up(router): project router added

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { authRouter } from "./feature/v1/identity/auth";
 import { userRouter } from "./feature/v1/identity/user";
+import { projectRouter } from "./feature/v1/progress/project";
 
 const router = Router();
 
 router.use("/identity/auth", authRouter);
 router.use("/identity/user", userRouter);
+router.use("/progress/project", projectRouter);
 
 export default router;
